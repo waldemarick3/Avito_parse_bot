8a1f05779953b2e7c031cbfa05722db84b5995fd
waldemarick3 <68539921+waldemarick3@users.noreply.github.com>
2023-04-20 16:44:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ TG_USER_ID = ""
 UPDATE_TIME = 60 * 2
 SETTINGS = {
     'url': 'https://www.avito.ru/moskva/noutbuki/apple-ASgCAQICAUCo5A0U9Nlm?cd=1&s=104&p=1',
-    'count': 1,
     'version_main': 112,
     'items': ['m1', 'm2', "m1pro"],
     'max_price': 86000
